73a3b4b785bfa1cc593b2364da6bc112d582b688
Ben Glick <glick@lclark.edu>
2020-02-06 14:50:09 -0800

    more cleaning up, separating out old postprocessing to wait on new postprocessing

--- all_ben.py
+++ all_ben.py
@@ -1,11 +1,10 @@
+#!/usr/local/bin/env python
+
 import os
 import sys
-import math
-import time
-import fnmatch
 
 import parsl
-from parsl.app.app import python_app, bash_app
+from parsl.app.app import bash_app
 from parsl.config import Config
 from parsl.providers import GridEngineProvider
 from parsl.executors import HighThroughputExecutor
@@ -14,23 +13,28 @@ from parsl.addresses import address_by_route
 from data_generation import generate_data
 
 config = Config(
-    executors=[HighThroughputExecutor(worker_debug=True,cores_per_worker=16,
-                                      address=address_by_route(), 
-                                      provider=GridEngineProvider( walltime='10000:00:00',nodes_per_block=1,
-                                                                  init_blocks=1, 
-                                                                   max_blocks=4, scheduler_options="#$ -pe smp 48"), 
+    executors=[HighThroughputExecutor(worker_debug=True,
+                                      cores_per_worker=16,
+                                      address=address_by_route(),
+                                      provider=GridEngineProvider(walltime='10000:00:00',
+                                                                  nodes_per_block=1,
+                                                                  init_blocks=1,
+                                                                  max_blocks=4,
+                                                                  scheduler_options="#$ -pe smp 48"
+                                                                  ),
                                       label="workers")
-              ],
+               ],
 )
 
 
-parsl.set_stream_logger() 
+parsl.set_stream_logger()
 parsl.load(config)
+
 
 @bash_app
 def run_single_index(filename, directory, threshold=1000):
     import os
-    
+
     mvalue = str(f"{filename}").split("/n")[0].split('.fasta')[0]
     genomeDir = f"{directory}{mvalue}/gd"
     if not os.path.isdir(f"{directory}/{mvalue}"):
@@ -38,87 +42,12 @@ def run_single_index(filename, directory, threshold=1000):
     if not os.path.isdir(genomeDir):
         os.makedirs(genomeDir)
 
-    os.chdir(f"{directory}/{mvalue}")  #change directory to mvalue folder we just made
+    # change directory to mvalue folder we just made
+    os.chdir(f"{directory}/{mvalue}")
 
     indexingstar = f'STAR --runThreadN 1 --runMode genomeGenerate --genomeDir  "{genomeDir}" --genomeFastaFiles "{directory}{filename}" --genomeSAindexNbases 2'
     return indexingstar
 
-                
-def run_single_align(filename):    
-    output = 0
-
-    uniquely = os.popen("awk 'FNR == 9{print $6}' " + strsvalue + "Log.final.out").read()
-    if uniquely == 0 : 
-        uniquely = '1'
-    uniquely = int(uniquely)
-
-    multi = os.popen("awk 'FNR == 24{print $9}' " + strsvalue + "Log.final.out").read()
-    if multi == '0': 
-        multi = '1'
-    multi = int(multi)
-
-    totalreads = os.popen("awk 'FNR == 6{print $6}' " + strsvalue+ "Log.final.out").read()
-
-    if uniquely + multi >= threshold:
-        os.system("echo " + filename + f">> {directory}/final.txt") 
-        output = 1
-        uniquelyC = 'NO'
-        multiC = 'NO'
-        totalreadsC = 'NO'
-        percRatio = 'NO'
-
-    if svalue <12: 
-        counter = 1
-    else: 
-        counter = 15
-
-    while output  == 0 and counter > 0:
-        os.system("echo" + filename + f" >> {directory}/maybehopper.txt")
-        if counter == svalue: 
-            counter +=1
-        if counter == 20 or counter == 7: 
-            counter = 0
-        elif counter == 17: 
-            counter = 19
-
-        strcounter = str(counter)
-
-        star_align(directory, mvalue, strcounter, genomeDir)
-
-        threshold = 1000
-
-        uniquelyC = os.popen("awk 'FNR == 9{print $6}' " + strcounter + "Log.final.out").read()
-        if uniquelyC == '0': 
-            uniquelyC = '1'
-        uniquelyC = int(uniquelyC)
-
-        multiC =  os.popen("awk 'FNR == 24{print $9}' " + strcounter + "Log.final.out").read()
-        if multiC == '0': 
-            multiC = '1'
-        multiC = int(multiC)
-
-        totalreadsC = os.popen("awk 'FNR == 6{print $6}' " + strcounter + "Log.final.out").read()
-
-        percRatio = (multiC + uniquelyC)/(multi + uniquely)
-        
-        if (multiC + uniquelyC) > threshold and percRatio > 100:
-            output = 1
-            os.system("echo " + filename + f" >> {directory}/hopper.txt")
-        else:
-            if counter == 17: 
-                counter = 19
-            elif counter == 20 or counter == 7: 
-                counter = 0
-            else: 
-                counter = counter + 1
-
-
-    if output == 1: #it needs to be added to a file
-        os.system("echo " + str(filename) + "  " +  str(uniquely) + "  " + str(multi) + "  " + str(totalreads) + "  " +  str(uniquelyC) + "  " +   str(multiC) + "  " + str(totalreadsC) + "  " +  str(percRatio)   + f" >> {directory}/index_hopping_output.txt")
-
-
-    if output == 0:
-        os.system("echo " + filename + f" >> {directory}/never.txt")
 
 @bash_app
 def star_align(filename, directory, inputs=[]):
@@ -131,27 +60,29 @@ def star_align(filename, directory, inputs=[]):
     svalue = str(int(str(filename.split("_")[0].split('s')[1])))
 
     genomeDir = f"{directory}{mvalue}/gd"
-    
-    outfilenameprefix = directory + mvalue +"/"+ svalue
+
+    outfilenameprefix = directory + mvalue + "/" + svalue
 
     if int(svalue) < 10:
         fullS = "s00" + str(svalue)
     else:
         fullS = "s0" + str(svalue)
-    
-    for rrfile in os.listdir('/home/users/ellenrichards/binfordlab/raw_reads/'):
+
+    for rrfile in os.listdir(
+            '/home/users/ellenrichards/binfordlab/raw_reads/'):
         if fnmatch.fnmatch(rrfile, "*" + fullS + "*R1*.fastq"):
             rawread1 = rrfile
         if fnmatch.fnmatch(rrfile, "*" + fullS + "*R2*.fastq"):
             rawread2 = rrfile
 
-    alignstar= f'STAR --runMode alignReads --runThreadN 16 --genomeDir "{genomeDir}"  --readFilesIn /home/users/ellenrichards/binfordlab/raw_reads/"{rawread1}"  /home/users/ellenrichards/binfordlab/raw_reads/"{rawread2}" --outFileNamePrefix "{outfilenameprefix}" --outSAMtype BAM SortedByCoordinate --limitBAMsortRAM 40000000000 --outTmpDir "{directory}/{mvalue}/align_tmp/"'
+    alignstar = f'STAR --runMode alignReads --runThreadN 16 --genomeDir "{genomeDir}"  --readFilesIn /home/users/ellenrichards/binfordlab/raw_reads/"{rawread1}"  /home/users/ellenrichards/binfordlab/raw_reads/"{rawread2}" --outFileNamePrefix "{outfilenameprefix}" --outSAMtype BAM SortedByCoordinate --limitBAMsortRAM 40000000000 --outTmpDir "{directory}/{mvalue}/align_tmp/"'
 
     return alignstar
 
+
 def parsl_first_align(directory):
     files = [f.strip() for f in open(f"{directory}/filenames.txt").readlines()]
-    
+
     # Start the indexing processes
     print("starting indexing")
     index_futures = []
@@ -160,18 +91,21 @@ def parsl_first_align(directory):
     # Wait for the indexing to finish
     index_futures = [i.result() for i in index_futures]
     print("done indexing")
-    
+
     # Start the alignment processes
     align_futures = []
     for index, file in enumerate(files):
-        align_futures.append(star_align(file, directory, inputs=[index_futures[index]]))
+        align_futures.append(
+            star_align(
+                file, directory, inputs=[
+                    index_futures[index]]))
     # Wait for the alignment to finish
     align_futures = [a.result() for a in align_futures]
-    
+
+
 def setup():
     proteomefile = sys.argv[1]
     directory = f'/home/users/ellenrichards/{sys.argv[2]}/'
-    threshold = 1000
 
     if not os.path.isdir(directory):
         os.makedirs(directory)
@@ -183,11 +117,12 @@ def setup():
     os.system(f"touch {directory}/never.txt")
     os.system(f"touch {directory}/maybehopper.txt")
     os.system(f"touch {directory}/index_hopping_output.txt")
-    os.system(f"echo 'filename  uniquely  multi  totalReads  uniquelyAGAINST  multiAGAINST  totalreadsAGAINST  percRatio'  >> {directory}/index_hopping_output.txt")
+    os.system(
+        f"echo 'filename  uniquely  multi  totalReads  uniquelyAGAINST  multiAGAINST  totalreadsAGAINST  percRatio'  >> {directory}/index_hopping_output.txt")
     return directory
-    
-if __name__ == "__main__":
-    
-    directory = setup()
-    parsl_first_align(directory) # Blocking call waits on all first aligns
 
+
+if __name__ == "__main__":
+
+    directory = setup()
+    parsl_first_align(directory)  # Blocking call waits on all first aligns
